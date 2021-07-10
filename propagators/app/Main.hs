ce3f412075e9f30fb87cfcee9869d1edd0691487
mniip <mniip@mniip.com>
2021-07-10 13:19:34 +0300

    Prefer combinations where a vertex is in the corner

--- propagators/app/Main.hs
+++ propagators/app/Main.hs
@@ -5,6 +5,10 @@ import Control.Monad
 import Data.IORef
 import System.Environment
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Set as S
+import qualified Data.IntMap.Strict as IM
+import Data.List (foldl')
+import Control.Concurrent.Async
 
 import ICFPC.Propagators
 import ICFPC.JSON
@@ -18,17 +22,32 @@ main = do
   let !vertices = (\(Pair x y) -> (x, y)) <$> figVertices (prFigure problem)
   let !edges = [(u, v, distSeg (vertices !! u, vertices !! v)) | Pair u v <- figEdges $ prFigure problem]
   let !eps = prEpsilon problem
+  let !numVs = length vertices
 
   bestRef <- newIORef Nothing
 
-  circ <- vertexCircuit eps boundary edges (length vertices)
-  iterateCircuit circ $ \vs -> do
-    if isValid eps boundary (map (\(u, v, _) -> (u, v)) edges) vertices vs
-    then do
-      let !score = dislikes boundary vs
-      isBest <- atomicModifyIORef' bestRef $ \mBest -> if maybe True (> score) mBest then (Just score, True) else (mBest, False)
-      when isBest $ do
-        putStrLn $ solFile <> ": New best: " <> show score
-        BSL.writeFile solFile $ encodeSolution $ Solution [Pair x y | (x, y) <- vs]
-    else putStrLn $ solFile <> ": Invalid (!?) " <> show vs
+  circ <- vertexCircuit eps boundary edges numVs
+  runCircuit circ
+  let
+    report vs =  do
+      if isValid eps boundary (map (\(u, v, _) -> (u, v)) edges) vertices vs
+      then do
+        let !score = dislikes boundary vs
+        isBest <- atomicModifyIORef' bestRef $ \mBest -> if maybe True (> score) mBest then (Just score, True) else (mBest, False)
+        when isBest $ do
+          putStrLn $ solFile <> ": New best: " <> show score
+          BSL.writeFile solFile $ encodeSolution $ Solution [Pair x y | (x, y) <- vs]
+      else putStrLn $ solFile <> ": Invalid (!?) " <> show vs
+  forConcurrently_ [0..numVs-1] $ \i ->
+    forConcurrently_ boundary $ \p -> do
+      circ' <- cloneCircuit circ
+      triggerNode circ' i (Finite $ S.singleton p)
+      iterateCircuit circ' report
+  putStrLn $ solFile <> ": Finished corner placements"
+  circ' <- cloneCircuit circ
+  masks <- viewNodes circ'
+  forM_ (IM.toList masks) $ \(i, s) -> case s of
+    Full -> pure () -- unreachable
+    Finite s -> triggerNode circ' i $ Finite $ foldl' (flip S.delete) s boundary
+  iterateCircuit circ' report
   putStrLn $ solFile <> ": Finished search"
